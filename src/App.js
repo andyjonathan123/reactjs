a4c5181666df55df2f88cd730e73f7fee304acff
andyjonathan123 <jonathanpakpahan1234@gmail.com>
2022-02-21 19:12:17 +0700

    form handling

--- src/App.js
+++ src/App.js
@@ -1,32 +1,67 @@
 import React, { Component } from "react";
-import axios from "axios";
+
 
 class App extends Component {
-  constructor () {
+  constructor() {
     super()
 
     this.state = {
-       user : []
+      firstname : '',
+      lastname  : '',
+      name      : ''
+
     }
   }
 
-  getUser = async () => {
-    let response = await axios.get('https://jsonplaceholder.typicode.com/users/1')
+  changeHandler = (e) =>{
     this.setState({
-      user : response.data
+      [e.target.name] : e.target.value
     })
   }
 
-  componentDidMount(){
-    this.getUser()
+  submitHandler = (e) => {
+    e.preventDefault()
+    this.setState({
+      name : `${this.state.firstname}  ${this.state.lastname}`
+    })
+    
+    this.state.firstname = ''
+    this.state.lastname = ''
+    
   }
 
-
   render() {
+    
+    const {name} = this.state
     return (
-      <div>
-       <div>menggunakan data {this.state.user.name}</div>
-      </div>
+      <div className="p-5">
+         <div className="container">
+           <div className="row">
+             <div className="col-md-4">
+               <div className="card">
+                 <div className="card-header">Belajar React JS</div>
+                 <div className="card-body">
+                   <form action="" onSubmit={this.submitHandler}>
+                     <div className="mb-4">
+                       <label htmlFor="firstname" className="form-label">firstname</label>
+                       <input type="text"name="firstname" id="" className="form-control" onChange={this.changeHandler} value={this.state.firstname}/>
+                     </div>
+                     
+                     <div className="mb-4">
+                       <label htmlFor="lastname" className="form-label">lastname</label>
+                       <input type="text"  name="lastname" id="" value={this.state.lastname} onChange={this.changeHandler} className="form-control" />
+                     </div>
+                     <button type="submit" className="btn btn-primary btn-block">show</button>
+                   </form>
+                 </div>
+                 <div className="card-footer">
+                    my name is {name ? name : '. . .'}
+                  </div>
+               </div>
+             </div>
+           </div>
+         </div>
+       </div>
     );
   }
 }
